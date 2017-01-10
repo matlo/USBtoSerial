7c286b47f72c4c729decb1f6871b75fbc5da6fef
Mathieu Laurendeau <mat.lau@laposte.net>
2017-01-10 13:39:50 +0100

    Add reboot to bootloader for Arduino boards.

--- USBtoSerial.c
+++ USBtoSerial.c
@@ -39,6 +39,18 @@
 
 #include "USBtoSerial.h"
 
+// Bootloader related fields
+// Old Caterian bootloader places the MAGIC key into unsafe RAM locations (it can be rewritten
+// by the running sketch before to actual reboot).
+// Newer bootloaders, recognizable by the LUFA "signature" at the end of the flash, can handle both
+// the usafe and the safe location. Check once if the bootloader in new, then set the
+// _updatedLUFAbootloader variable to true/false and place the magic key consequently
+#define MAGIC_KEY 0x7777
+#define MAGIC_KEY_POS 0x0800
+#define NEW_LUFA_SIGNATURE 0xDCFB
+
+static bool _updatedLUFAbootloader = false;
+
 /** Circular buffer to hold data from the serial port before it is sent to the host. */
 static RingBuffer_t USARTtoUSB_Buffer;
 
@@ -144,6 +156,12 @@ void SetupHardware(void)
 
 	TCCR1B |= ((1 << CS10) | (1 << CS11)); // Set up timer at FCPU /64
 
+#if MAGIC_KEY_POS != (RAMEND-1)
+	if (pgm_read_word(FLASHEND - 1) == NEW_LUFA_SIGNATURE) {
+		_updatedLUFAbootloader = true;
+	}
+#endif
+
 	/* Hardware Initialization */
 	LEDs_Init();
 	USB_Init();
@@ -200,12 +218,79 @@ ISR(USART1_RX_vect, ISR_BLOCK)
 	RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
 }
 
+/* Borrowed from the Arduino source code:
+ * https://github.com/arduino/Arduino/blob/2bfe164b9a5835e8cb6e194b928538a9093be333/hardware/arduino/avr/cores/arduino/CDC.cpp#L97
+ */
+static void handleResetToBootloader(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo)
+{
+	// auto-reset into the bootloader is triggered when the port, already
+	// open at 1200 bps, is closed.  this is the signal to start the watchdog
+	// with a relatively long period so it can finish housekeeping tasks
+	// like servicing endpoints before the sketch ends
+
+	uint16_t magic_key_pos = MAGIC_KEY_POS;
+
+// If we don't use the new RAMEND directly, check manually if we have a newer bootloader.
+// This is used to keep compatible with the old leonardo bootloaders.
+// You are still able to set the magic key position manually to RAMEND-1 to save a few bytes for this check.
+#if MAGIC_KEY_POS != (RAMEND-1)
+	// For future boards save the key in the inproblematic RAMEND
+	// Which is reserved for the main() return value (which will never return)
+	if (_updatedLUFAbootloader) {
+		// horray, we got a new bootloader!
+		magic_key_pos = (RAMEND-1);
+	}
+#endif
+
+	// We check DTR state to determine if host port is open (bit 0 of lineState).
+	if (CDCInterfaceInfo->State.LineEncoding.BaudRateBPS == 1200 && (CDCInterfaceInfo->State.ControlLineStates.HostToDevice & CDC_CONTROL_LINE_OUT_DTR) == 0)
+	{
+#if MAGIC_KEY_POS != (RAMEND-1)
+		// Backup ram value if its not a newer bootloader.
+		// This should avoid memory corruption at least a bit, not fully
+		if (magic_key_pos != (RAMEND-1)) {
+			*(uint16_t *)(RAMEND-1) = *(uint16_t *)magic_key_pos;
+		}
+#endif
+		// Store boot key
+		*(uint16_t *)magic_key_pos = MAGIC_KEY;
+		wdt_enable(WDTO_120MS);
+	}
+	else
+	{
+		// Most OSs do some intermediate steps when configuring ports and DTR can
+		// twiggle more than once before stabilizing.
+		// To avoid spurious resets we set the watchdog to 250ms and eventually
+		// cancel if DTR goes back high.
+
+		wdt_disable();
+		wdt_reset();
+#if MAGIC_KEY_POS != (RAMEND-1)
+		// Restore backed up (old bootloader) magic key data
+		if (magic_key_pos != (RAMEND-1)) {
+			*(uint16_t *)magic_key_pos = *(uint16_t *)(RAMEND-1);
+		} else
+#endif
+		{
+			// Clean up RAMEND key
+			*(uint16_t *)magic_key_pos = 0x0000;
+		}
+	}
+}
+
+void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo)
+{
+	handleResetToBootloader(CDCInterfaceInfo);
+}
+
 /** Event handler for the CDC Class driver Line Encoding Changed event.
  *
  *  \param[in] CDCInterfaceInfo  Pointer to the CDC class interface configuration structure being referenced
  */
 void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo)
 {
+	handleResetToBootloader(CDCInterfaceInfo);
+
 	uint8_t ConfigMask = 0;
 
 	switch (CDCInterfaceInfo->State.LineEncoding.ParityType)
